02b90649d4e9c4cf028daa4ee480ebf01d1ca0c2
Prasanta Dutta <prasanta7dutta@gmail.com>
2022-05-02 16:00:43 +0530

    Removed functions from Question 2

--- Assignment2/Question2.c
+++ Assignment2/Question2.c
@@ -7,14 +7,6 @@
 #include<math.h>
 #include<time.h>
 
-int move(){
-    return rand() & 1 ? -1 : 1;
-}
-
-int distance(int a,int b){
-    return abs(b-a);
-}
-
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
@@ -27,9 +19,9 @@ int main(int argc, char const *argv[])
     for (int i = 1; i <= k; i++) {
         int x = 0;
         for (int j = 0; j < n; j++) {
-            x += move();
+            x += rand() & 1 ? -1 : 1;
         }
-        D += distance(0, x) * distance(0, x);
+        D += abs(x) * abs(x);
     }
     printf("D_E = %lf\n", sqrt(D/k));
     return 0;
