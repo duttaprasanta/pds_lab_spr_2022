218d9e1d867d358db7e9de4db737d686c0d5d7b4
nishkalprakash <nishkalprakash@gmail.com>
2022-05-21 18:49:10 +0530

    + Modified `LT1_Q2.c`

--- LT1/LT1_Q2.c
+++ LT1/LT1_Q2.c
@@ -1,48 +1,35 @@
-#include<stdio.h>
-#include<math.h>
-#include <stdlib.h>
+/*
+Coder: Nishkal Prakash
 
-float f(float x)
-{
-	return pow(x,3) - 25;
-}
+Working Example 1:
+	Enter X_0, Eplsilon, MaxIteration: 5 0.005 10
 
-float derivative(float x) 
-{
-	return 3*pow(x,2);
-}
+	Iteration       x_0             x_1             Error
+	1               5.000000        3.666667        1.333333
+	2               3.666667        3.064279        0.602388
+	3               3.064279        2.930340        0.133939
+	4               2.930340        2.924031        0.006309
+	5               2.924031        2.924018        0.000014
+	The square root is: 2.924018
+*/
 
-void main()
+#include <stdio.h>
+#include <math.h>
+
+int main()
 {
-    float x, epsilon, error, x_prev, error_abs;
-    int max_iter, i=0;
-    
-    printf("\nEnter the value of x: ");
-    scanf("%f", &x);
-    
-    printf("\nEnter the value of epsilon: ");
-    scanf("%f", &epsilon);
-    
-    printf("\nEnter the value of maximun number of itrations: ");
-    scanf("%d", &max_iter);
-    
-    printf("\nIteration  x_0  x_1  Error ");
-    while(i <= max_iter) 
-    {
-		x_prev = x;
-		
-		x = x - f(x)/derivative(x);
-		error = x - x_prev;
-		if(error<0)
-		  error_abs = -error;
-		else
-		  error_abs = error;  
-		
-		printf("\n%d    %f    %f    %f", i+1, x_prev, x, error_abs);
-		if(error_abs < epsilon)
-		  break;
-		
-		i++;
-	}
-	printf("\nThe square root is: %f\n", x);
+	double x0, ep, err, x1;
+	int mi, i = 0;
+
+	printf("\nEnter X_0, Eplsilon, MaxIteration: ");
+	scanf("%lf %lf %d", &x0, &ep, &mi);
+	printf("\n%-10s\t%-8s\t%-8s\t%-8s", "Iteration", "x_0", "x_1", "Error");
+	do
+	{
+		x1 = x0 - (pow(x0, 3) - 25) / (3 * pow(x0, 2));
+		err = fabs(x1 - x0);
+		printf("\n%-10d\t%-8lf\t%-8lf\t%-8lf", i + 1, x0, x1, err);
+		x0 = x1;
+	} while (err > ep && ++i < mi);
+	printf("\nThe square root is: %lf\n", x1);
 }
